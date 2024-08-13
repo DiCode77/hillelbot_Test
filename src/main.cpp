165b28021c23c421fbbca0f80fa79666d9a082ee
DiCode77 <DiCode77>
2024-08-13 21:34:43 +0300

    changed main

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 int main() {
     // Виконати GET-запит до публічного API
-    cpr::Response r = cpr::Get(cpr::Url{"https://jsonplaceholder.typicode.com/todos/1"});
+    cpr::Response r = cpr::Get(cpr::Url{"https://raw.githubusercontent.com/DiCode77/hillelbot_Test/main/MyRequest.json"});
 
     // Вивести статус код відповіді
     std::cout << "Status Code: " << r.status_code << std::endl;
@@ -15,10 +15,8 @@ int main() {
     nlohmann::json jsonResponse = nlohmann::json::parse(r.text);
 
     // Вивести певне поле з JSON відповіді
-    std::cout << "Title: " << jsonResponse["title"] << std::endl;
 
-    std::string str = jsonResponse["title"];
-    fmt::print("{}\n", str);
+    fmt::print("{}\n", jsonResponse["title"].get<std::string>());
     fmt::print("Program end!\n");
 
     return 0;
