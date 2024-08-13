e05798c57f7ee776a08ad8c4a63bf4706a62593b
DiCode77 <DiCode77>
2024-08-13 20:33:59 +0300

    updated code, added support for nlohmann-json and cpr libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <cpr/cpr.h>
+#include <nlohmann/json.hpp>
 
-int main (){
-    std::cout << "Hello World!" << std::endl;
+int main() {
+    // Виконати GET-запит до публічного API
+    cpr::Response r = cpr::Get(cpr::Url{"https://jsonplaceholder.typicode.com/todos/1"});
+
+    // Вивести статус код відповіді
+    std::cout << "Status Code: " << r.status_code << std::endl;
+
+    // Парсити JSON відповідь
+    nlohmann::json jsonResponse = nlohmann::json::parse(r.text);
+
+    // Вивести певне поле з JSON відповіді
+    std::cout << "Title: " << jsonResponse["title"] << std::endl;
+
+    return 0;
 }
